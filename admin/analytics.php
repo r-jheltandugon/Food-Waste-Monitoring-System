447dfe932e62ed17a8501eb52d09277df28ceb4e
ambo18 <jherietandugon@gmail.com>
2024-08-10 22:53:32 +0800

    Update charts

--- admin/analytics.php
+++ admin/analytics.php
@@ -57,10 +57,6 @@ if($_SESSION['name']==''){
                     <i class="uil uil-heart"></i>
                     <span class="link-name">Donates</span>
                 </a></li>
-                <li><a href="feedback.php">
-                    <i class="uil uil-comments"></i>
-                    <span class="link-name">Feedbacks</span>
-                </a></li>
                 <li><a href="adminprofile.php">
                     <i class="uil uil-user"></i>
                     <span class="link-name">Profile</span>
@@ -96,14 +92,8 @@ if($_SESSION['name']==''){
         <div class="top">
             <i class="uil uil-bars sidebar-toggle"></i>
             <!-- <p>Food Donate</p> -->
-            <p  class ="logo" >Food <b style="color: #06C167; ">Donate</b></p>
+            <p  class ="logo" ><b style="color: #06C167; ">Food Waste Monitoring System</b></p>
              <p class="user"></p>
-            <!-- <div class="search-box">
-                <i class="uil uil-search"></i>
-                <input type="text" placeholder="Search here...">
-            </div> -->
-            
-            <!--<img src="images/profile.jpg" alt="">-->
         </div>
 
         <div class="dash-content">
@@ -126,17 +116,6 @@ if($_SESSION['name']==''){
                         ?>
                         <!-- <span class="number">50,120</span> -->
                     </div>
-                    <div class="box box2">
-                        <i class="uil uil-comments"></i>
-                        <span class="text">Feedbacks</span>
-                        <?php
-                           $query="SELECT count(*) as count FROM  user_feedback";
-                           $result=mysqli_query($connection, $query);
-                           $row=mysqli_fetch_assoc($result);
-                         echo "<span class=\"number\">".$row['count']."</span>";
-                        ?>
-                        <!-- <span class="number">20,120</span> -->
-                    </div>
                     <div class="box box3">
                         <i class="uil uil-heart"></i>
                         <span class="text">Total doantes</span>
@@ -157,71 +136,86 @@ if($_SESSION['name']==''){
 <canvas id="donateChart" style="width:100%;max-width:600px"></canvas>
 
 <script>
-      <?php
-            $query="SELECT count(*) as count FROM  login where gender=\"male\"";
-            $q2="SELECT count(*) as count FROM  login where gender=\"female\"";
-            $result=mysqli_query($connection, $query);
-            $res2=mysqli_query($connection, $q2);
-            $row=mysqli_fetch_assoc($result);
-            $ro2=mysqli_fetch_assoc($res2);
-            $female=$ro2['count'];
-            $male=$row['count'];
-            $q3="SELECT count(*) as count FROM food_donations where location=\"madurai\"";
-            $res3=mysqli_query($connection, $q3);
-            $ro3=mysqli_fetch_assoc($res3);
-            $madurai=$ro3['count'];
-            $q4="SELECT count(*) as count FROM food_donations where location=\"chennai\"";
-            $res4=mysqli_query($connection, $q4);
-            $ro4=mysqli_fetch_assoc($res4);
-            $chennai=$ro4['count'];
-            $q5="SELECT count(*) as count FROM food_donations where location=\"coimbatore\"";
-            $res5=mysqli_query($connection, $q5);
-            $ro5=mysqli_fetch_assoc($res5);
-            $coimbatore=$ro5['count'];
-            
+<?php
+    // Database queries to fetch counts
+    $query = "SELECT count(*) as count FROM login WHERE gender='male'";
+    $q2 = "SELECT count(*) as count FROM login WHERE gender='female'";
+    $result = mysqli_query($connection, $query);
+    $res2 = mysqli_query($connection, $q2);
+    $row = mysqli_fetch_assoc($result);
+    $ro2 = mysqli_fetch_assoc($res2);
+    $male = intval($row['count']);
+    $female = intval($ro2['count']);
 
-    ?>
-var xValues = ["Male","Female"];
-var xplace=["Madurai","chennai","coimbatore"];
-var yplace=[<?php echo json_encode($madurai,JSON_HEX_TAG);?>,<?php echo json_encode($coimbatore,JSON_HEX_TAG);?>,<?php echo json_encode($chennai,JSON_HEX_TAG);?>];
-var yValues = [<?php echo json_encode($male,JSON_HEX_TAG);?>,<?php echo json_encode($female,JSON_HEX_TAG);?>,30];
-var barColors = ["#06C167","blue"];
-var bar=["#06C167","blue","red"]
+    $q3 = "SELECT count(*) as count FROM food_donations WHERE category='bio'";
+    $res3 = mysqli_query($connection, $q3);
+    $ro3 = mysqli_fetch_assoc($res3);
+    $bio = intval($ro3['count']);
+
+    $q4 = "SELECT count(*) as count FROM food_donations WHERE category='nonbio'";
+    $res4 = mysqli_query($connection, $q4);
+    $ro4 = mysqli_fetch_assoc($res4);
+    $nonbio = intval($ro4['count']);
+?>
+
+var xValues = ["Male", "Female"];
+var xplace = ["Biodegradable", "Non-Biodegradable"];
+var yplace = [<?php echo $bio; ?>, <?php echo $nonbio; ?>];
+var yValues = [<?php echo $male; ?>, <?php echo $female; ?>];
+var barColors = ["#06C167", "blue"];
+var bar = ["red", "blue"];
 
 new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    title: {
-      display: true,
-      text: "User details"
+    type: "bar",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+    },
+    options: {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: "User Details"
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    stepSize: 1 // This will ensure the y-axis shows whole numbers only
+                }
+            }
+        }
     }
-  }
 });
 
+// Chart for Food Donation Details
 new Chart("donateChart", {
-  type: "bar",
-  data: {
-    labels: xplace,
-    datasets: [{
-      backgroundColor: bar,
-      data: yplace
-    }]
-  },
-  options: {
-    legend: {display: false},
-    title: {
-      display: true,
-      text: "food donation details"
+    type: "bar",
+    data: {
+        labels: xplace,
+        datasets: [{
+            backgroundColor: bar,
+            data: yplace
+        }]
+    },
+    options: {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: "Food Donation Details"
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    stepSize: 1 
+                }
+            }
+        }
     }
-  }
 });
 </script>
 
