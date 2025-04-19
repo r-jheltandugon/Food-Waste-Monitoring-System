2c7103cfc01bbb3a65257543cda28dad36733e9e
ambo18 <jherietandugon@gmail.com>
2025-04-19 12:33:17 +0800

    Upload updated database

--- database/demo.sql
+++ database/demo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2024 at 11:46 AM
+-- Generation Time: Apr 19, 2025 at 06:27 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,7 +41,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`Aid`, `name`, `email`, `password`, `location`, `address`) VALUES
-(7, 'Admin', 'admin@gmail.com', '$2y$10$SxJyat1uyMzxR27x0Ze/.OvM6ZuLEhpysnmVSSBN7ZzC29AgXcJI6', 'madurai', 'Guian, Eastern Samar');
+(9, 'Admin2', 'admin@gmail.com', '$2y$10$NYCyDL5GTWPr0P75CkgTn.6WZLMmkeFPRBpy0mf8hHl0iCkzJ7kSe', 'madurai', 'None');
 
 -- --------------------------------------------------------
 
@@ -77,6 +77,7 @@ CREATE TABLE `food_donations` (
   `food` varchar(50) NOT NULL,
   `type` text NOT NULL,
   `category` text NOT NULL,
+  `classification` varchar(20) DEFAULT NULL,
   `quantity` text NOT NULL,
   `date` datetime DEFAULT current_timestamp(),
   `address` text NOT NULL,
@@ -90,10 +91,15 @@ CREATE TABLE `food_donations` (
 -- Dumping data for table `food_donations`
 --
 
-INSERT INTO `food_donations` (`Fid`, `name`, `email`, `food`, `type`, `category`, `quantity`, `date`, `address`, `location`, `phoneno`, `assigned_to`, `delivery_by`) VALUES
-(26, 'R-jhel Tandugon', 'jherietandugon@gmail.com', 'Adobo', 'veg', 'bio', '10', '2024-06-18 12:40:22', 'Brgy.Seguinon Salcedo Eastern Samar', 'chennai', '9971478896', NULL, NULL),
-(27, 'tests', 'jherietandugon@gmail.com', 'Kalabasa', '', 'bio', '50kg', '2024-08-10 21:49:36', 'Brgy. 3 Guian, E. S.', '', '0912123324', 5, NULL),
-(28, 'Trexie Danco', 'test@gmail.com', 'Vegetables', '', 'bio', '100 kg', '2024-08-20 17:19:52', 'Brgy.  09 Guian, E.S.', '', '0965765646', NULL, NULL);
+INSERT INTO `food_donations` (`Fid`, `name`, `email`, `food`, `type`, `category`, `classification`, `quantity`, `date`, `address`, `location`, `phoneno`, `assigned_to`, `delivery_by`) VALUES
+(27, 'tests', 'jherietandugon@gmail.com', 'Kalabasa', '', 'bio', NULL, '50kg', '2024-08-10 21:49:36', 'Brgy. 3 Guian, E. S.', '', '0912123324', 5, NULL),
+(28, 'Trexie Danco', 'test@gmail.com', 'Vegetables', '', 'bio', NULL, '100 kg', '2024-08-20 17:19:52', 'Brgy.  09 Guian, E.S.', '', '0965765646', NULL, NULL),
+(29, 'asdf', 'asdf@gmail.com', 'Pechay', '', 'nonbio', NULL, '200', '2024-11-24 20:48:24', 'Bargy.8 ', '', '0909304938', NULL, NULL),
+(30, 'test', 'test@gmail.com', 'test food', '', 'bio', NULL, '12', '2024-11-24 21:28:16', 'test brgy', '', '09098787878', NULL, NULL),
+(31, 'test', 'test@gmail.com', 'Mcdo', '', 'bio', 'fertilizer', '100', '2025-03-20 19:17:21', 'Public Market  qwerty street', '', '09324798798', NULL, NULL),
+(32, 'test', 'test@gmail.com', 'test fertilizer', '', 'bio', 'fertilizer', '12', '2025-03-20 20:06:37', 'dfdsf', '', '09788672352', NULL, NULL),
+(33, 'test', 'test@gmail.com', 'test fertilizer 2', '', 'bio', 'fertilizer', '120', '2025-03-20 20:06:59', 'dfdsf', '', '09788672352', NULL, NULL),
+(34, 'test', 'test@gmail.com', 'test fertilizer 2', '', 'bio', 'pet_food', '120', '2025-03-20 20:07:11', 'dfdsf', '', '09788672352', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -114,6 +120,7 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `name`, `email`, `password`, `gender`) VALUES
+(25, 'asdf', 'asdf@gmail.com', '$2y$10$jM.D49sqJW2CHqK1eZm24.DcD3AH.OOzfrGU2gzKUE0128KtgPdf.', 'female'),
 (21, 'rjhel', 'jherietandugon@gmail.com', '$2y$10$P5wALb6wAiC604oI6yJ5eeptvhbGFi3tx4OqO9TSjySL.bg0Q2SbO', 'male'),
 (23, 'R-jhel', 'qwerty@gmail.com', '$2y$10$HzMGdz/fz3IACRs/nXSbGOpNAmA9eBACNwbE3tsyX/ap3REvQAtiK', 'male'),
 (24, 'test', 'test@gmail.com', '$2y$10$JqyaC96Wsjiix1ongH9VvO3KcK0WmOQVbbZfar4wHvDjbRtSzcrE.', 'male');
@@ -188,7 +195,7 @@ ALTER TABLE `user_feedback`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `Aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `Aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `delivery_persons`
@@ -200,13 +207,13 @@ ALTER TABLE `delivery_persons`
 -- AUTO_INCREMENT for table `food_donations`
 --
 ALTER TABLE `food_donations`
-  MODIFY `Fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `Fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `user_feedback`
